e7046544112b257c97e90a82711111e5e5404bd7
richness1911 <ganny1911@gmail.com>
2022-10-13 10:59:54 -0700

    4-puts.c new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
