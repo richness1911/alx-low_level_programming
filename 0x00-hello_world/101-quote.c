e29add1f199454d53d897835621789c2eb2cc11a
richness1911 <ganny1911@gmail.com>
2022-10-13 12:23:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main
+ * main - Entry
  * Return: Always 1
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
 }
